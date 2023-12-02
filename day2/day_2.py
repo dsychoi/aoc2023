ffcc4cb8f93b6d9c1eb8a2c533355212780ed37a
dsychoi <dsychoi@gmail.com>
2023-12-02 02:19:01 -0600

    rename methods

--- day2/day_2.py
+++ day2/day_2.py
@@ -1,6 +1,6 @@
 from urllib import request
-import os
 from dotenv import load_dotenv
+import os
 import re
 load_dotenv()
 
@@ -9,10 +9,6 @@ token = os.getenv('aoc_token')
 
 req = request.Request(
     url, headers={'Cookie': f'session={token}'})
-
-
-test_str = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
-# Convert into a dictionary with game # as key and list of 3-tuples as values representing RGB
 
 
 def get_clean_line(line):
@@ -33,7 +29,7 @@ def get_dict_item(game_number, game_sets):
     return {game_number: values}
 
 
-def get_clean_data():
+def get_dict():
     games = {}
 
     with request.urlopen(req) as f:
@@ -53,17 +49,17 @@ def remove_invalid_games(games, r, g, b):
     return games
 
 
-def get_minimum_needed(game_sets):
-    max_red, max_green, max_blue = 0, 0, 0
+def get_minimum_colors_needed(game_sets):
+    red, green, blue = 0, 0, 0
     for game_set in game_sets:
-        max_red = max(max_red, game_set[0])
-        max_green = max(max_green, game_set[1])
-        max_blue = max(max_blue, game_set[2])
-    return (max_red, max_green, max_blue)
+        red = max(red, game_set[0])
+        green = max(green, game_set[1])
+        blue = max(blue, game_set[2])
+    return (red, green, blue)
 
 
 def get_sum_of_ids():
-    games = get_clean_data()
+    games = get_dict()
     games = remove_invalid_games(games, 12, 13, 14)
     return sum(games.keys())
 
@@ -73,10 +69,10 @@ def power_of_tuple(rgb):
 
 
 def get_sum_of_set_powers():
-    games = get_clean_data()
+    games = get_dict()
     sum = 0
     for game in games.values():
-        sum += power_of_tuple(get_minimum_needed(game))
+        sum += power_of_tuple(get_minimum_colors_needed(game))
     return sum
 
 
